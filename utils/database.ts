e7014309946cf6f3f24d861e2f120bf99e017ffa
Piyu0423 <piumikamadubashini5@gmail.com>
2025-03-04 00:27:56 +0800

    Mongoose  DB connectivity

---
+++ utils/database.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+let isConnected: boolean = false;
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+    });
+    isConnected = true;
+    console.log("MongoDB is connected");
+  } catch (error) {
+    console.log("Error connecting to MongoDB: ", error);
+  }
+};
