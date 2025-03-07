ca916b2a73394b814d70caf37405dfbec4496eda
Piyu0423 <piumikamadubashini5@gmail.com>
2025-03-07 11:59:02 +0800

    fixed import issue

--- app/create-prompt/page.tsx
+++ app/create-prompt/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Form from "@components/Form";
 
